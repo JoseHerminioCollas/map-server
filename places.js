4d2da63c760574e5bbc32854f39d44bba6f55ee1
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2019-11-05 13:37:04 -0800

    correct message to user

--- places.js
+++ places.js
@@ -3,7 +3,7 @@ const places = (googleMapsPlaces) => {
   return (
     async function placesReturn(req, res) {
       if (!req.query || !req.query.q) {
-        res.send('No query needs to be provided')
+        res.send('A query needs to be provided')
       }
       else {
         const query = req.query.q
