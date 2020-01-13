175e8266b945c66b1d158a5791ffab92a66a569a
Jose Collas <joseherminiocollas@goatstonc.com>
2020-01-13 07:02:14 -0800

    update url param names

--- places.js
+++ places.js
@@ -3,12 +3,12 @@ const places = (googleMapsPlaces) => {
   return (
     async function placesReturn(req, res) {
       // query latLng radiuss
-      const query = req.query.q
-      const location = req.query.latlng
+      const query = req.query.query
+      const location = req.query.location
       const radius = req.query.radius
-      if (!req.query ||
-        !req.query.q ||
-        !req.query.latlng ||
+      if (!req.query |
+        !req.query.query ||
+        !req.query.location ||
         !req.query.radius) {
         res.send('A user query needs a query, lat : long, and radius to be provided')
       }
